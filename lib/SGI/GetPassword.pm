40b9f9cbdc76f8cfac9281a61170f9a2bd0dbc2b
Chris Robert Tonini <chrisrtonini@gmail.com>
2017-10-31 16:39:34 -0200

    Ajustes na documentação

--- lib/SGI/GetPassword.pm
+++ lib/SGI/GetPassword.pm
@@ -12,40 +12,59 @@ use warnings FATAL => 'all';
 use Term::ReadKey;
 use FileHandle;
 
-=encoding cp850
+=encoding utf8
 
 =head1 NOME
 
-SGI::GetPassword - Utilitario para coleta de senhas via prompt (caracter).
+SGI::GetPassword - UtilitE<aacute>rio para coleta de senhas via prompt 
+(caracter).
 
-=head1 VERSAE<Atilde>O
+=head1 VERSE<Atilde>O
 
-VersE<atilde>o 0.01
+VersE<atilde>o 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SINOPSE
 
-Classe estE<aacute>tica que implementa mecanismo para coleta de senhas, 
-ignorando caracteres especiais, processando apenas BACKSPACE e ENTER. 
-Retorna a senha digitada em formato texto, no entanto, durante a coleta, 
-exibe apenas "*" na tela.
+Classe estE<aacute>tica que implementa mecanismo para coleta de senhas. Onde,
+a cada caracter digitado, apresenta apenas "*" na tela. Ignora caracteres
+especiais, processando apenas I<backspace>/I<del> e I<enter>.
+Retorna a senha digitada, em formato texto.
 
-O canal de I<echo> e coleta e B<I<STDERR>>, e a tecla ENTER finaliza a leitura.
+Exemplo de uso:
 
     use SGI::GetPassword;
+    
+    ...
     
     my $password = SGI::GetPassword::Get();
     
     ...
     
 ou
+    ...
     
     my $password = SGI::GetPassword::Get("Informe a senha: ");
+    
     ...
+    
+
+B<IMPORTANTE>:
+
+=over 4
+
+=item * O canal de I<output> utilizado sempre serE<aacute> I<stderr>.
+
+=item * O E<uacute>nicos caracteres de controle processados sE<atilde>o: 
+ENTER, DEL e BACKSPACE (respectivamente, ASCII 10, 127 e 8). Demais teclas,
+cujos cE<oacute>digos de varredura sejam inferiores a ASCII 32 
+(espaE<ccedil>o) serE<atilde>o ignoradas.
+
+=back
     
 
 =head1 ME<Eacute>TODOS
@@ -54,15 +73,31 @@ ou
 
 =head2 Get()
 
-ME<eacute>todo que efetua a coleta da senha obscurecida, na linha de comando.
+Exibe o I<prompt> (opcional) e coleta a senha no terminal, obscurecendo sua 
+digitaE<ccedil>E<atilde>o.
 
-Adaptado de: 
+=head3 ParE<acirc>metro(s):
 
-	L<http://stackoverflow.com/questions/701078/how-can-i-enter-a-password-using-perl-and-replace-the-characters-with>
+=over 4
 
-Autor:
+=item * prompt
 
-	L<Pierre-Luc Simard|http://stackoverflow.com/users/68554/pierre-luc-simard>.
+Texto a ser exibido na tela (via I<stderr>) imediatamente antes da 
+solicitaE<ccedil>E<atilde>o da senha.
+
+=back
+
+=head3 CrE<eacute>ditos:
+
+=over 4
+
+ImplementaE<ccedil>E<atilde>o original: 
+L<http://stackoverflow.com/questions/701078/how-can-i-enter-a-password-using-perl-and-replace-the-characters-with>
+
+Autor: 
+L<Pierre-Luc Simard|http://stackoverflow.com/users/68554/pierre-luc-simard>.
+
+=back
 
 =cut
 
@@ -81,18 +116,26 @@ sub Get	{
 	# O laço de leitura continua até que a tecla ENTER seja pressionada
 	# (identificada pelo valor ASCII "10")
 	while (ord($key = ReadKey(0)) != 10)	{
+		
 		# Consulte valores para ord($key) em http://www.asciitable.com/
 		if (ord($key) == 127 || ord($key) == 8)	{
-			# DEL/BACKSPACE pressionado
+			
+			#           --== DEL/BACKSPACE pressionado ==--
+			
 			# 1) remove o último caracter da senha
 			# Obs.: "if" abaixo acrescentado por Chris
 			chop($password) if (length($password) > 0);
+			
 			# 2) retroceder o cursor em uma posição, imprimir um espaço em 
 			#		branco, e novamente retroceder o cursor uma posição
 			print STDERR "\b \b";
 		}
 		elsif (ord($key) < 32)	{
 			# Ignorar cacteres de controle
+			print chr(7);
+			
+			# TODO: substituir o 'chr(7)' (ASCII bel) pelo 
+			#		módulo Audio::Beep.
 		}
 		else	{
 			$password .= $key;
@@ -110,32 +153,28 @@ sub Get	{
 
 =head1 AUTOR
 
-Chris Robert Tonini, C<< <chrisrtonini at gmail.com> >>
+Chris Robert Tonini, C<< <chrisrtoniniE<64>gmail.com> >>
 
 
 =head1 BUGS
 
-Favor reportar qualquer bug ou necessidade de modificaE<ccedil>E<atilde>o a
-C<chrisrtonini at gmail.com>, ou pela interface do ServiE<ccedil>o de
-Atendimento ao Cliente (SAC) no endereE<ccedil>o 
-L<http://sac.sgisistemas.com.br>. Eu serei notificado e retornarei 
-informaE<ccedil>E<otilde>es sobre o andamento das 
-modificaE<ccedil>E<otilde>es realizadas.
+Qualquer necessidade de implementaE<ccedil>E<atilde>o ou registro de I<bug>
+pode ser informado no repositE<oacute>rio do projeto:
+L<https://github.com/chrisrtonini/SGI-GetPassword>.
 
 
 =head1 SUPORTE
 
-E<Eacute> possE<iacute>vel encontrar documentaE<ccedil>E<atilde>o para este
-mE<oacute>dulo com o comando perldoc.
-
-    perldoc SGI::GetPassword
-
-
-VocE<ecirc> pode tambE<eacute>m buscar informaE<ccedil>E<otilde>es em:
+InformaE<ccedil>E<otilde>es sobre este mE<oacute>dulo podem ser obtidas de
+duas maneiras.
 
 =over 4
 
-=item * RepositE<oacute>rio de fontes do projeto
+=item * Pelo comando perldoc:
+
+    perldoc SGI::GetPassword
+
+=item * Acessando o repositE<oacute>rio do projeto:
 
 L<https://github.com/chrisrtonini/SGI-GetPassword>
 
@@ -144,23 +183,25 @@ L<https://github.com/chrisrtonini/SGI-GetPassword>
 
 =head1 DECLARAE<Ccedil>E<Otilde>ES
 
-
 =head1 LICENE<Ccedil>A E COPYRIGHT
 
 Copyright 2015 Chris Robert Tonini.
 
-Este programa e' software livre; voce pode redistribui-lo e/ou
-modifica-lo sob os termos da GNU Lesser General Public License,
-publicada pela Free Software Foundation; em sua versao 2.1
+Este programa E<eacute> I<software> livre; vocE<ecirc> pode 
+redistribuE<iacute>-lo e/ou modificE<aacute>-lo sob os termos da 
+B<GNU Lesser General Public License>, publicada pela 
+Free Software Foundation; em sua versE<atilde>o 2.1
 
-Este programa e' distribuido na esperanca de que lhe seja util,
-mas SEM QUALQUER GARANTIA; nem mesmo a garantia implicita de
-COMERCIALIZACAO ou ADAPTACAO A UM PROPOSITO PARTICULAR.  Veja
-a GNU Lesser General Public License para mais detalhes.
+Este programa E<eacute> distribuE<iacute>do na esperanE<ccedil>a de que lhe 
+seja E<uacute>til, mas B<SEM QUALQUER GARANTIA>; nem mesmo a garantia 
+implE<iacute>cita de B<COMERCIALIZAE<Ccedil>E<Atilde>O> ou 
+B<ADAPTAE<Ccedil>E<Atilde>O A UM PROPE<Oacute>SITO PARTICULAR>. Leia
+a B<GNU Lesser General Public License> para mais detalhes.
 
-Caso voce nao tenha recebido uma copia da GNU Lesser General
-Public License juntamente com este programa, e' possivel obte-la
-no endereco: http://www.gnu.org/copyleft/lesser.html.
+Caso vocE<ecirc> nE<atilde>o tenha recebido uma cE<oacute>pia da 
+B<GNU Lesser General Public License> juntamente com este programa, 
+E<eacute> possE<iacute>vel obtE<ecirc>-la no endereE<ccedil>o: 
+L<http://www.gnu.org/copyleft/lesser.html>.
 
 =cut
 
